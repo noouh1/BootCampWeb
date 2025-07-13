ed74b3d5317f6dc0d9bb23e35e15c2bd39fc4434
noouh1 <ae2303@fayoum.edu.eg>
2025-07-13 19:28:26 +0300

    [Add]:problems from V to Z

---
+++ Day-17/Problems/W.cs
@@ -0,0 +1,14 @@
+﻿string[] inputs = Console.ReadLine().Split(' ');
+int n = int.Parse(inputs[0]);
+char c = char.Parse(inputs[1]);
+int m = int.Parse(inputs[2]);
+char d = char.Parse(inputs[3]);
+long result = long.Parse(inputs[4]);
+
+Console.WriteLine(
+    c == '+'? (n + m == result ? "Yes" : n+m) :
+    c == '-'? (n - m == result ? "Yes" : n-m) :
+    c == '*'? (n * m == result ? "Yes" : n*m) :
+    "invalid operation"
+    
+    );
