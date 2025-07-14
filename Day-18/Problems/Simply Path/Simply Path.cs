b23382e92f19fd2e8e495eeb4acac4b8a762595b
noouh1 <ae2303@fayoum.edu.eg>
2025-07-14 04:58:35 +0300

    [Add]:problems

---
+++ Day-18/Problems/Simply Path/Simply Path.cs
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    public string SimplifyPath(string path) {
+        Stack<string> stack = new Stack<string>();
+        string[] parts = path.Split('/');
+
+        foreach (string part in parts) {
+            if (part == "" || part == ".") {
+                continue;
+            } else if (part == "..") {
+                if (stack.Count > 0)
+                    stack.Pop(); 
+            } else {
+                stack.Push(part); 
+            }
+        }
+
+        var result = new List<string>(stack);
+        result.Reverse();
+
+        return "/" + string.Join("/", result);
+    }
+}
---
+++ Day-18/Problems/list to binary tree/list to binary tree.cs
@@ -0,0 +1,56 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode SortedListToBST(ListNode head) {
+        if (head == null) return null;
+
+        ListNode mid = FindMiddleNode(head);
+        TreeNode root = new TreeNode(mid.val);
+
+        if (head == mid) return root;
+
+        root.left = SortedListToBST(head);
+        root.right = SortedListToBST(mid.next);
+
+        return root;
+    }
+
+    private ListNode FindMiddleNode(ListNode head) {
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (prev != null)
+            prev.next = null;
+
+        return slow; 
+    }
+}
