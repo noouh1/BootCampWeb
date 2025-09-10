6162170e8b5d9e4510c0b3af2b71e6e44d2f0766
noouh1 <ae2303@fayoum.edu.eg>
2025-09-10 18:04:01 +0300

    Add Validators

--- Day-28/WebApplication1/WebApplication1/Features/Course/Query/Models/GetAllCourseDto.cs
+++ Day-28/WebApplication1/WebApplication1/Features/Course/Query/Models/GetAllCourseDto.cs
@@ -5,5 +5,6 @@ namespace WebApplication1.Features.Student.Query.Models;
 
 public class GetAllCourseDto : IRequest<Response>
 {
-    
+    public string Cname { get; set; }
+    public int Hours { get; set; }
 }
---
+++ Day-28/WebApplication1/WebApplication1/Features/Student/Validators/UpdateStudentValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using WebApplication1.Features.Student.Command.Models;
+
+namespace WebApplication1.Features.Course.Command.Validators;
+
+public class UpdateStudentValidator : AbstractValidator<UpdateStudentDto>
+{
+    public UpdateStudentValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("Student Id is required");
+    }
+}
